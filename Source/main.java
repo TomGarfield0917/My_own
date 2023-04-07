9cc07a83555d1917f03cbd591fa577ceb45c3150
Tom Garfield <t0mg4rfield7117@gmail.com>
2023-04-07 16:08:44 +0700

    Update and rename main.java to main.c

--- Source/main.java
+++
@@ -1,9 +0,0 @@
-Meow Meow. It's Tom time muahaha.
- 
-Oh wait my name is similar to that stupid director.
-  
-Meow Meow. It's Cat time muahaha.
-
-I hate all dogs and humans in this world. I somehow managed to sneak into this dog research facility and steal a mutant of theirs.
-  
-Now i will use this appearance to control all that stupid humans and dogs.
