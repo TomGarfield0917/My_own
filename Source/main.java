953493e14056abae0d2e3b97c22e232259376b45
Tom Garfield <130136665+TomGarfield0917@users.noreply.github.com>
2023-04-07 15:03:44 +0700

    Create main.java

---
+++ Source/main.java
@@ -0,0 +1,29 @@
+    public void printDog() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("            /~~~~~~~~\\                           _");
+        System.out.println("    ##\\__/ @)      ~~~~~~~~\\                     \\ \\ ) )");
+        System.out.println("    |              /~~\\~~~~~                ((    |  \\");
+        System.out.println("     \\    /           |                          /   |");
+        System.out.println("      (~~~   /         \\____________/~~~~~~~~~~~~   /");
+        System.out.println("       ~~~~|~                                     /");
+        System.out.println("           :                                      |");
+        System.out.println("            \\                                     |");
+        System.out.println("            |                               /      \\");
+        System.out.println("             \\  \\_         :         \\     /~~~\\    |");
+        System.out.println("             /   :~~~~~|   :~~~~~~~~~~|   :     :   :");
+        System.out.println("            /    :    /    :         /    :    /    :");
+        System.out.println("        (~~~     )(~~~     )     (~~~     )(~~~     )");
+        System.out.println("         ~~~~~~~~  ~~~~~~~~       ~~~~~~~~  ~~~~~~~~");
+        System.out.println("          STOMP     STOMP          STOMP     STOMP");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+    }
